a854af9d8bfa5a2c5635de7d77a3634bdcbe951b
Akib Ali <razaaqib7860@gmail.com>
2026-06-09 16:25:41 +0530

    Update comment
    Clarify comment about changing const variable.

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -4,7 +4,7 @@ var accountPassword = "12345"
 accountCity = "Jaipur"
 let accountState;
 
-// accountId = 2 // not allowed
+// accountId = 2 // not allowed because you changeing const variable
 
 
 accountEmail = "hc@hc.com"
